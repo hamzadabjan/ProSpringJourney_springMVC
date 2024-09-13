736128e593e96ccb1296cd0d168655a67705252a
Hamza <hamza.dabjan@gmail.com>
2024-09-14 02:02:24 +0300

    spring MVC Binding Request Params

--- src/main/java/com/hamza/springboot/training/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/hamza/springboot/training/thymeleafdemo/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -28,6 +29,21 @@ public class HelloWorldController {
 
         // read th request parameter from the HTML form
         String theName = request.getParameter("studentName");
+
+        // convert the data to Uppercase
+        theName=theName.toUpperCase();
+
+        // create the message
+        String result = "heyy "+ theName;
+
+        // add message to the model
+        model.addAttribute("message",result);
+
+        return "helloworld";
+    }
+
+    @GetMapping("/processFormVersionThree")
+    public String capitalizeName(@RequestParam("studentName") String theName, Model model){
 
         // convert the data to Uppercase
         theName=theName.toUpperCase();
