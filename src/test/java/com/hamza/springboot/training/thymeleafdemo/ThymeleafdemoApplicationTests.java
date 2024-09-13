9a73c4eac7933e841b52675566c9c09982395d27
Hamza <hamza.dabjan@gmail.com>
2024-09-13 03:19:27 +0300

    Thymeleaf with css and hello world Form and Model

---
+++ src/test/java/com/hamza/springboot/training/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamza.springboot.training.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
